00ceaa371ca9afaf6c992144e0b84e0d05c83b91
Vladislav Pak <mcrazy091@gmail.com>
2018-03-22 21:16:10 +0200

    add markup

--- resources/views/user/account_settings.blade.php
+++ resources/views/user/account_settings.blade.php
@@ -52,5 +52,46 @@
                 </form>
             </div>
         </div>
+
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-header">
+                    <b>Wallets addresses</b>
+                </div>
+                <form class="ajax_form_save" action="{{route('save-wallets_addresses')}}" data-title="Wallets addresses" method="post">
+                    {{csrf_field()}}
+                    <div class="card-body row">
+                        <div class="col-md-12">
+                            <div class="form-group">
+                                <label for="first_name">BTC</label>
+                                <input class="form-control" id="first_name" name="first_name" type="text"
+                                       placeholder="John">
+                            </div>
+                        </div>
+                        <div class="col-md-12">
+                            <div class="form-group">
+                                <label for="last_name">DOGE</label>
+                                <input class="form-control" id="last_name" name="last_name" type="text"
+                                       placeholder="John">
+
+                            </div>
+                        </div>
+                        <div class="col-md-12">
+                            <div class="form-group">
+                                <label for="ltc">LTC</label>
+                                <input class="form-control" id="ltc" name="ltc" type="text"
+                                       placeholder="abc@geekman.site">
+                            </div>
+                        </div>
+                        <div class="col-md-12">
+                            <div class="form-group text-right">
+                                <button class="btn btn-success" type="submit">Save</button>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+
     </div>
 @endsection
